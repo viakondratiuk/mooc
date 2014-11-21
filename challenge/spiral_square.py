1a050fb1dee2faeb278cbc5cdcf3b739c2b969b4
Vyacheslav Kondratyuk <v.kondratyuk@ism-ukraine.com>
2014-11-21 16:22:28 +0200

    Spiral square. Input size, output spiral square.

---
+++ challenge/spiral_square.py
@@ -0,0 +1,15 @@
+'''
+Function to create spiral squares.
+Input: square size
+Output: spiral square
+print spiral_square(4)
+[
+[1, 2, 3, 4],
+[12, 13, 14, 5],
+[11, 16, 15, 6],
+[10, 8, 9, 7],
+]
+'''
+
+def spiral_square(size):
+    res = []
