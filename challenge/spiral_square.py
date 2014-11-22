9c8c283985c6ff75be5df2f53c2477d6eb3210ef
Vyacheslav Kondratyuk <viakondratiuk@gmail.com>
2014-11-22 16:11:34 +0200

    Spiral_square: Some notes how to finish algorithm.

--- challenge/spiral_square.py
+++ challenge/spiral_square.py
@@ -9,6 +9,28 @@ print spiral_square(4)
 [11, 16, 15, 6],
 [10, 8, 9, 7],
 ]
+----
+The way how we should fill cells with numbers:
+row = 0, column = 0-3
+(0, 0), (0, 1), (0, 2), (0, 3),
+
+row = 1-3, column = 3
+(1, 3), (2, 3), (3, 3),
+
+row = 3, column = 2-0
+(3, 2), (3, 1), (3, 0),
+
+row = 2-1, column = 0
+(2, 0), (1, 0),
+
+row = 1, column = 1-2
+(1, 1), (1, 2),
+
+row = 2, column = 2
+(2, 2),
+
+row = 2, column = 1
+(2, 1)
 '''
 
 def spiral_square(size):
