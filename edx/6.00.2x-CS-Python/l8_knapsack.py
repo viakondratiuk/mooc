af07cef93bf3045a39326839f41f862e62df0feb
Viacheslav Kondratiuk <viakondratiuk@gmail.com>
2014-11-26 14:42:07 +0200

    6.00.2x-CS-Python: Yield sets in two bags.

--- edx/6.00.2x-CS-Python/l8_knapsack.py
+++ edx/6.00.2x-CS-Python/l8_knapsack.py
@@ -107,4 +107,24 @@ def testChoseBestSet():
     taken, val = choseBestSet(items, 20)
     print ('Total value of items taken = ' + str(val))
     for item in taken:
-        print '  ', item        
+        print '  ', item
+        
+def yieldAllCombos(items):
+    """
+    Generates all combinations of N items into two bags, whereby each item is in one or zero bags.
+    Yields a tuple, (bag1, bag2), where each bag is represented as a list of which item(s) are in each bag.
+    """
+    # Your code here
+    N = len(items)
+    for i in xrange(3**N):
+        bag1 = []
+        bag2 = []
+        for j in xrange(N):
+            if (i / 3**j) % 3 == 1: 
+                bag1.append(items[j])
+            elif (i / 3**j) % 3 == 2:
+                bag2.append(items[j])
+        yield (bag1, bag2)
+        
+for i in yieldAllCombos(items='ABC'):
+    print i                        
