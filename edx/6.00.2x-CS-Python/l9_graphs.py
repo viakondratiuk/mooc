5779bdc49487a3c77af91cd58ff86995e7d672e6
Viacheslav Kondratiuk <viakondratiuk@gmail.com>
2014-12-02 16:26:29 +0200

    6.00.2x-CS-Python: DFS starting algorithm.

--- edx/6.00.2x-CS-Python/l9_graphs.py
+++ edx/6.00.2x-CS-Python/l9_graphs.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class Node(object):
     def __init__(self, name):
         self.name = str(name)
@@ -68,4 +69,49 @@ class Graph(Digraph):
     def addEdge(self, edge):
         Digraph.addEdge(self, edge)
         rev = Edge(edge.getDestination(), edge.getSource())
-        Digraph.addEdge(self, rev)    
+        Digraph.addEdge(self, rev)
+        
+def testSP():
+    nodes = []
+    for name in range(6):
+        nodes.append(Node(str(name)))
+    g = Digraph()
+    for n in nodes:
+        g.addNode(n)
+    g.addEdge(Edge(nodes[0],nodes[1]))
+    g.addEdge(Edge(nodes[1],nodes[2]))
+    g.addEdge(Edge(nodes[2],nodes[3]))
+    g.addEdge(Edge(nodes[2],nodes[4]))
+    g.addEdge(Edge(nodes[3],nodes[4]))
+    g.addEdge(Edge(nodes[3],nodes[5]))
+    g.addEdge(Edge(nodes[0],nodes[2]))
+    g.addEdge(Edge(nodes[1],nodes[0]))
+    g.addEdge(Edge(nodes[3],nodes[1]))
+    g.addEdge(Edge(nodes[4],nodes[0]))
+    
+    return (g, nodes)
+    
+def printPath(path):
+    # a path is a list of nodes
+    result = ''
+    for i in range(len(path)):
+        if i == len(path) - 1:
+            result = result + str(path[i])
+        else:
+            result = result + str(path[i]) + '->'
+    return result    
+    
+def DFS(graph, start, end, path = []):
+    path = path + [start]
+    if start == end:
+        return path
+    for node in graph.childrenOf(start):
+        if node not in path:
+            newPath = DFS(graph, node, end, path)
+            if newPath != None:
+                return newPath
+    return None
+    
+g, nodes = testSP()
+sp = DFS(g, nodes[0], nodes[5])
+print 'Shortest path found by DFS:', printPath(sp)
