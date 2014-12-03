a83c3d539f853461405242a649cddd49e546a4fc
Viacheslav Kondratiuk <viakondratiuk@gmail.com>
2014-12-03 10:51:22 +0200

    6.00.2x-CS-Python: DFS Shortest and BFS.

--- edx/6.00.2x-CS-Python/l9_graphs.py
+++ edx/6.00.2x-CS-Python/l9_graphs.py
@@ -99,6 +99,7 @@ def printPath(path):
     
 def DFS(graph, start, end, path = []):
     path = path + [start]
+    print 'Current DFS path:', printPath(path)
     if start == end:
         return path
     for node in graph.childrenOf(start):
@@ -107,7 +108,40 @@ def DFS(graph, start, end, path = []):
             if newPath != None:
                 return newPath
     return None
+    
+def DFSShortest(graph, start, end, path = [], shortest = None):
+    #assumes graph is a Digraph
+    #assumes start and end are nodes in graph
+    path = path + [start]
+    print 'Current dfs path:', printPath(path)
+    if start == end:
+        return path
+    for node in graph.childrenOf(start):
+        if node not in path: #avoid cycles
+            if shortest == None or len(path) < len(shortest):
+                newPath = DFSShortest(graph,node,end,path,shortest)
+                if newPath != None:
+                    shortest = newPath
+    return shortest
+    
+def BFS(graph, start, end, q = []):
+    initPath = [start]
+    q.append(initPath)
+    while len(q) != 0:
+        tmpPath = q.pop(0)
+        lastNode = tmpPath[len(tmpPath) - 1]
+        print 'Current dequeued path:', printPath(tmpPath)
+        if lastNode == end:
+            return tmpPath
+        for linkNode in graph.childrenOf(lastNode):
+            if linkNode not in tmpPath:
+                newPath = tmpPath + [linkNode]
+                q.append(newPath)
+    return None    
 
 g, nodes = testSP()
-sp = DFS(g, nodes[0], nodes[5])
-print 'Shortest path found by DFS:', printPath(sp)
+#sp = DFSShortest(g, nodes[0], nodes[5])
+#print 'Shortest path found by DFS:', printPath(sp)
+
+p = BFS(g, nodes[0], nodes[5])
+print 'Shortest path found by BFS:', printPath(p)
