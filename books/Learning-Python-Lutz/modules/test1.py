ac4fdcf0362265db5eaed2e39d592ea9d9aac311
Viacheslav Kondratiuk <viakondratiuk@gmail.com>
2014-12-11 16:58:35 +0200

    Learning-Python-Lutz: _, __all__, __name__

---
+++ books/Learning-Python-Lutz/modules/test1.py
@@ -0,0 +1,6 @@
+from test2 import *
+
+
+if __name__ == '__main__':
+    a1()
+    print 'I\'m cool'
---
+++ books/Learning-Python-Lutz/modules/test2.py
@@ -0,0 +1,19 @@
+__all__ = ['a1', 'a2', '_a4']
+
+def a1():
+    print 'a1'
+    
+def a2():
+    print 'a2'
+    
+def a3():
+    print 'a3'
+    
+def _a4():
+    print 'a4'
+    
+def a5():
+    print 'a5'
+    
+if __name__ == 'test2':
+    print 'Cool dude'
